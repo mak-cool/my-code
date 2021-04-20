a2b861e5c4100826df2c643766b5451e13217024
mak-cool <68188109+mak-cool@users.noreply.github.com>
2021-04-20 16:09:27 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-whoami
+#whoami
+#pwd
+#sshpass -p '123456' ssh -o StrictHostKeyChecking=no  ssh-user@35.224.153.219
+#cd /home/makcool205
+#ls -l
+#touch sshfile.txt
+#ls -l
+
+
+RUN ssh -i id_rsa test@35.226.111.194
 pwd
-sshpass -p '123456' ssh -o StrictHostKeyChecking=no  ssh-user@35.224.153.219
-cd /home/makcool205
-ls -l
-touch sshfile.txt
-ls -l
+cd /home/makcool205/
+bash value.sh
